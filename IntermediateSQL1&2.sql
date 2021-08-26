3beac2c4c925471079246969c8e0a941d558bffe
Xinyi Luo <eaco1996c@outlook.com>
2021-08-26 13:02:43 -0400

    Update and rename IntermediateSQL.sql to IntermediateSQL1&2.sql

--- IntermediateSQL1&2.sql
+++ IntermediateSQL1&2.sql
@@ -1,8 +1,12 @@
+
+--Section 1. We'll take the CASE
+
+
 -- Identify the home team as Bayern Munich, Schalke 04, or neither
 SELECT 
-	CASE when hometeam_id = 10189 then 'FC Schalke 04'
-        when hometeam_id = 9823 then 'FC Bayern Munich'
-         ELSE 'Other' END AS home_team,
+	CASE WHEN hometeam_id = 10189 then 'FC Schalke 04'
+             WHEN hometeam_id = 9823 then 'FC Bayern Munich'
+             ELSE 'Other' END AS home_team,
 	COUNT(id) AS total_matches
 FROM matches_germany
 -- Group by the CASE statement alias
@@ -117,6 +121,134 @@ FROM country AS c
 LEFT JOIN matches AS m
 ON c.id = m.country_id
 GROUP BY country;
+
+
+-- Section 2. Short and Simple Subqueries
+
+
+SELECT 
+	-- Select the date, home goals, and away goals scored
+    date,
+	home_goal,
+	away_goal
+FROM  matches_2013_2014
+-- Filter for matches where total goals exceeds 3x the average
+WHERE (home_goal + away_goal) > 
+       (SELECT 3 * AVG(home_goal + away_goal)
+        FROM matches_2013_2014); 
+	
+	
+SELECT 
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team 
+-- Exclude all values from the subquery
+WHERE team_api_id NOT IN
+     (Select DISTINCT hometeam_ID  FROM match);
+     
+     
+     
+SELECT
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Filter for teams with 8 or more home goals
+WHERE team_api_id IN
+	  (SELECT hometeam_id
+       FROM match
+       WHERE home_goal >= 8);
+       
+       
+       
+SELECT
+	-- Select country name and the count match IDs
+    name AS country_name,
+    COUNT(sub.id) AS matches
+FROM country AS c
+-- Inner join the subquery onto country
+-- Select the country id and match id columns
+inner join (SELECT country_id, id 
+           FROM match 
+           -- Filter the subquery by matches with 10+ goals
+           WHERE (home_goal + away_goal) >= 10) AS sub
+ON c.id = sub.country_id
+GROUP BY country_name;
+
+
+
+
+SELECT
+	-- Select country, date, home, and away goals from the subquery
+    country,
+    date,
+    home_goal,
+    away_goal
+FROM 
+	-- Select country name, date, home_goal, away_goal, and total goals in the subquery
+	(SELECT c.name AS country, 
+     	    m.date, 
+     		m.home_goal, 
+     		m.away_goal,
+           (home_goal + m.away_goal) AS total_goals
+    FROM match AS m
+    LEFT JOIN country AS c
+    ON m.country_id = c.id) AS subq
+-- Filter by total goals scored in the main query
+WHERE total_goals  >= 10;
+
+
+
+SELECT 
+	l.name AS league,
+    -- Select and round the league's total goals
+    ROUND(avg(m.home_goal + m.away_goal), 2) AS avg_goals,
+    -- Select & round the average total goals for the season
+    (SELECT round(avg(home_goal + away_goal), 2) 
+     FROM match
+     where season = '2013/2014') AS overall_avg
+FROM league AS l
+LEFT JOIN match AS m
+ON l.country_id = m.country_id
+-- Filter for the 2013/2014 season
+WHERE season = '2013/2014'
+GROUP BY league;
+
+
+
+SELECT
+	-- Select the league name and average goals scored
+	l.name AS league,
+	ROUND(avg(m.home_goal + m.away_goal),2) AS avg_goals,
+    -- Subtract the overall average from the league average
+	ROUND(AVG(m.home_goal + m.away_goal) -
+		(SELECT AVG(home_goal + away_goal)
+		 FROM match 
+         WHERE season = '2013/2014'),2) AS diff
+FROM league AS l
+LEFT JOIN match AS m
+ON l.country_id = m.country_id
+-- Only include 2013/2014 results
+WHERE season = '2013/2014'
+GROUP BY l.name;
+
+
+
+SELECT 
+	-- Select the stage and average goals for each stage
+	m.stage,
+    ROUND(AVG(m.home_goal + m.away_goal),2) AS avg_goals,
+    -- Select the average overall goals for the 2012/2013 season
+    ROUND((SELECT AVG(home_goal + away_goal) 
+           FROM match 
+           WHERE season = '2012/2013'),2) AS overall
+FROM match AS m
+-- Filter for the 2012/2013 season
+WHERE season = '2012/2013'
+-- Group by stage
+GROUP BY m.stage;
+
 
 
 
